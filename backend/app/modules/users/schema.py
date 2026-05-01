54329c9669099a6ff5bab2aa7e4e16a67c9610f0
Ryuse-i <pychopomp70@gmail.com>
2026-05-01 18:06:07 +0800

    add auth frontend

--- backend/app/modules/users/schema.py
+++ backend/app/modules/users/schema.py
@@ -3,15 +3,18 @@ from fastapi_users import schemas
 
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
+    username: str
+    full_name: str | None
     # what gets returned when you read a user
-    pass
 
 
 class UserCreate(schemas.BaseUserCreate):
+    username: str
+    full_name: str | None = None
     # what's required to register (email + password minimum)
-    pass
 
 
 class UserUpdate(schemas.BaseUserUpdate):
+    username: str | None = None
+    full_name: str | None = None
     # what fields can be updated
-    pass
